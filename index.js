53b9bd09c76c3e1d9d44df0b2c425d4e7d4cda7a
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-02-23 04:41:35 -0500

    clean up js code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var hamburgerBtn = document.querySelector(".hamburger-btn")
-var menu = document.querySelector(".menu")
 var dropDownMenu = document.querySelector(".hamburger-menu")
 
 
@@ -15,19 +14,16 @@ hamburgerBtn.onclick = function() {
     if (dropDownMenu.classList.contains("show-menu")){
         hamburgerBtn.setAttribute("aria-expanded", true); //true open menu
     } else {
-        hamburgerBtn.setAttribute("aria-expanded", false); //false close menu
-        
+        hamburgerBtn.setAttribute("aria-expanded", false); //false close menu  
     }
 }  
-console.log('submited')
-
     //close the dropdown menu when click off menu
 document.body.onclick = function(e) {
-    //if (hamburgerBtn.getAttribute("aria-expanded" === true)) {
+    
     if (e.target === hamburgerBtn || dropDownMenu.contains(e.target))  {
         return }
-
-    dropDownMenu.classList.remove("show-menu") // use classList to return the class form as an array.
+    // use classList to return the class form as an array.
+    dropDownMenu.classList.remove("show-menu") 
     
     //focus on button for event
     hamburgerBtn.focus()
@@ -35,11 +31,11 @@ document.body.onclick = function(e) {
 }
 
 document.onkeyup = function(e) {
-       if (e.key !== "Escape")
-        return
-        hamburgerBtn.setAttribute("aria-expanded", false);
-        dropDownMenu.classList.remove("show-menu") // use classList to return the class form as an array.
-        hamburgerBtn.focus()
+    if (e.key !== "Escape")
+    return
+    hamburgerBtn.setAttribute("aria-expanded", false);
+    dropDownMenu.classList.remove("show-menu") 
+    hamburgerBtn.focus()
 }
 
              
